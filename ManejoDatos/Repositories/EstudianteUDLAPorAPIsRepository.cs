f51870c212f376822e99296ad3d59162ccae735b
Wikiniki15 <wiki.niki.olalla@gmail.com>
2024-12-01 16:36:31 -0500

    Clase miércoles

---
+++ ManejoDatos/Repositories/EstudianteUDLAPorAPIsRepository.cs
@@ -0,0 +1,46 @@
+﻿using ManejoDatos.Interfaces;
+using ManejoDatos.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoDatos.Repositories
+{
+    public class EstudianteUDLAPorAPIsRepository : IEstudianteUDLARepository
+    {
+        public string _urlEndpoint = "https://freetestapi.com/api/v1/students";
+        public bool ActualizarEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CrearEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EstudianteUDLA> DevuelveEstadoEstudiantes()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(_urlEndpoint).Result;
+                var json_data = response.Content.ReadAsStringAsync().Result;
+                List<EstudianteAPI> estudiantesAPI = JsonConvert.DeserializeObject<List<EstudianteAPI>>(json_data);
+
+            }
+        }
+
+        public EstudianteUDLA DevuelveEstudianteUDLA(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarEstudianteUDLA(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
